b1f68ff3dca50402cdf2b61eb545767765cd3ed8
Naman-Gayakwad <110586020+Naman-Gayakwad@users.noreply.github.com>
2023-02-15 09:42:50 +0530

    all

--- product/models.py
+++ product/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=55)
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     added_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True) # Change uploads to thumbnails 
